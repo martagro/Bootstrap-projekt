5c52c3c8ddf4e92f89b93d546211282d79adf74f
Marta Gronkiewicz <martamc@o2.pl>
2018-02-04 16:55:25 +0100

    bez stopki

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//efekt zanikania po kliknięciu w link, który przenosi na inną stronę 
 $('a[href*="http"]').click(function (e) {
     e.preventDefault();
     console.log('test');
@@ -6,6 +7,30 @@ $('a[href*="http"]').click(function (e) {
     $('body').fadeOut(5000, function () {
         window.location.href = url;
     });
+});
 
 
+$(window).scroll(function () {
+
+    var scrollTop = $(this).scrollTop();
+
+    //przezroczysty niebieski pasek menu po scrollu
+    if (scrollTop > 70) {
+        $('.navbar').addClass('scroll');
+    } else {
+        $('.navbar').removeClass('scroll');
+    }
+
+    //Progress bar 
+    var percent = (scrollTop * 100) / ($('body').height() - $(this).height());
+    $('.progressbar').css({
+        width: (percent + '%')
+    });
+});
+
+$('a[href*="#"]').click(function () {
+    var anchor = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(anchor).offset().top - 70
+    }, 1000);
 });
